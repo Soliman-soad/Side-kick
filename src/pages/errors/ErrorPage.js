2da917fcf66a78c004d7899e5bfcabc5f6865327
Soliman-soad <mdsolimansoad@gmail.com>
2022-12-10 14:59:11 +0600

    error page updated

--- src/pages/errors/ErrorPage.js
+++ src/pages/errors/ErrorPage.js
@@ -5,8 +5,8 @@ import errorImg from "../../image/errorPage.jpg"
 const ErrorPage = () => {
     return (
         <div >
-            <img src={errorImg} className='w-2/5 mx-auto' alt='errorPage'/>
-            <Link to='/' className='btn px-4 py-3 mx-auto relative left-2/4 rounded-md text-white bg-blue-600'>Go back to home</Link>
+            <img src="https://media4.giphy.com/media/n4xLW9k2tLOw1rt1VF/giphy.gif?cid=ecf05e47ual8ik647dfugrofu0su1ecnm8h5rxjlw1tgn1es&rid=giphy.gif&ct=s" className='w-2/5 mx-auto' alt='errorPage'/>
+            <Link to='/' className='btn px-4 py-3 mx-auto relative left-2/4 rounded-md text-white bg-teal-600'>Go back to home</Link>
         </div>
     );
 };
--- src/routers/PrivateRouter.js
+++ src/routers/PrivateRouter.js
@@ -1,13 +1,24 @@
 import React, { useContext } from 'react';
 import { Navigate, useLocation } from 'react-router-dom';
 import { ProfileContext } from '../context/UserContext';
+import { Audio, Triangle } from  'react-loader-spinner'
+
+
 
 const PrivateRouter = ({children}) => {
     let location = useLocation();
     const {user,loading} = useContext(ProfileContext);
     if(loading){
         // loader section
-        return <div className="w-16 h-16 border-4 border-dashed rounded-full animate-spin dark:border-violet-400"></div>
+        return <div className='mx-auto mt-10'><Triangle
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="triangle-loading"
+        wrapperStyle={{}}
+        wrapperClassName=""
+        visible={true}
+      /></div>
     }
     if (user && user.uid){
         return children;
